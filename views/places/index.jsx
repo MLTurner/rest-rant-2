ef590a52190af9a45ddf6bcb0bc0568713ba937c
MLTurner <mlturne3@ncsu.edu>
2022-05-30 12:44:41 -0400

    adding places, new, show, edit views

---
+++ views/places/index.jsx
@@ -0,0 +1,57 @@
+const React = require('react')
+const Def = require('../default')
+
+
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
+
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
+  
+  router.post('/', (req, res) => {
+    res.send('POST /places stub')
+})
+
+router.get('/new', (req, res) => {
+    res.send('GET /places/new stub')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+    res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+    res.send('GET /places/:id/edit stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+    res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = index
