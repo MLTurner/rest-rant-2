043efc97065baa0ca79ebd9f074c5e8a084b7dd7
MLTurner <mlturne3@ncsu.edu>
2022-05-29 20:14:17 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+//requiring express
 const express = require('express')
+//initializing app variable
 const app = express ()
+//connecting to .env file
+require('dotenv').config()
 
+//GET homepage route
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000)
+//GET 404 page route
+//404 status code is from express
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+//telling our app to listen to our encrypted port
+app.listen(process.env.PORT)
